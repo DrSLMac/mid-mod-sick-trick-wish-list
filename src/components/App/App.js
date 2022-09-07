5fe1c7869727e45ac9c3e1f1c31cadb7f1f9fa0b
Shauna MacFarlane-Okongo <shauna.macfarlane579@gmail.com>
2022-09-07 10:47:50 -0500

    initial commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,25 @@
 import { Component } from 'react';
 import './App.css';
+import { fetchAllTricks } from '../../apiCalls'
+import Tricks from '../Tricks'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      tricks: [], //array of 3 objects
+    }
+  }
+
+  componentDidMount = () => {
+    fetchAllTricks().then(data => this.setState({ tricks: data }))
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Sick Trick Wish List</h1>
+        <Tricks />
       </div>
     );
   }
---
+++ src/components/Tricks.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Tricks.css'
+
+const Tricks = (props) => {
+    return (
+        <h2>Tricks go here!</h2>
+    )
+}
+
+export default Tricks
